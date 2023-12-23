d47978c3147597cc9df8645779091c96d9631814
Artem Bondarchuk <arhipilagys@gmail.com>
2023-12-23 19:09:10 +1100

    done

--- script/script.js
+++ script/script.js
@@ -3,6 +3,10 @@ const cartBlock = document.querySelector('.cart-main')
 const burger = document.querySelector('.burger')
 const burgerOpenBtn = document.querySelector('.burger-open-btn')
 const burgerCloseBtn = document.querySelector('.burger-close-btn')
+const form = document.querySelector('.form')
+const popup = document.querySelector('.popup-container')
+const popupCloseBtn = document.querySelector('.popup-btn')
+const formInputs = form.querySelectorAll('.search-input')
 
 cartBtn.addEventListener('mouseover', () => {
     cartBlock.classList.remove('disabled')
@@ -21,4 +25,20 @@ burgerCloseBtn.addEventListener('click', () => {
     burger.classList.add('disabled')
     document.body.classList.remove('no-scroll')
 })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    popup.classList.remove('disabled')
+    formInputs.forEach((formInput) => {
+        formInput.value = ''
+    })
+    document.body.classList.add('no-scroll')
+})
+
+popupCloseBtn.addEventListener('click', () => {
+    popup.classList.add('disabled')
+    document.body.classList.remove('no-scroll')
+})
+
+
 
